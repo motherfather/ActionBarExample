6ed2310a6fea29efd4677a0f98b4d3b82849ee13
motherfather <father@mail.com>
2016-11-29 10:07:09 +0900

    완성

--- app/src/main/java/com/bit2016/actionbarexample/MainActivity.java
+++ app/src/main/java/com/bit2016/actionbarexample/MainActivity.java
@@ -2,6 +2,9 @@ package com.bit2016.actionbarexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemid = item.getItemId();
+        String menuTitle = item.getTitle().toString();
+        switch (itemid) {
+            case R.id.item1 : {
+                Toast.makeText(this, menuTitle + " selected", Toast.LENGTH_SHORT).show();
+                break;
+            }
+            case R.id.item2 : {
+                Toast.makeText(this, menuTitle + " selected", Toast.LENGTH_SHORT).show();
+                break;
+            }
+            case R.id.item3 : {
+                Toast.makeText(this, menuTitle + " selected", Toast.LENGTH_SHORT).show();
+                break;
+            }
+            case R.id.item4 : {
+                Toast.makeText(this, menuTitle + " selected", Toast.LENGTH_SHORT).show();
+                break;
+            }
+            default:
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
